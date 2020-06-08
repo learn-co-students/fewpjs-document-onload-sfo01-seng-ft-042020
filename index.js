7626fd80b1b849c76a132fe604d582b6afdc8b4f
narduc <radnielpadron@gmail.com>
2020-06-08 12:22:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    document.querySelector("#text").innerHTML = "This is really cool!";
+  });
+
+
+
+//   id="text"
+
+//   document.addEventListener('keydown', function(e) {
+//     if (e.key === "ArrowRight") {
+//       moveDodgerRight()
+//     }
+//   })
+
+
+
+//   /* Enter the code to remove the node element here */
+// document.querySelector("main#main").remove();
+
+// /* Create your new element here and assign it to newHeader */
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory"
+// newHeader.innerHTML = "Flatiron School is the champion!";
+// newHeader.className = "victory";
